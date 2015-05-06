fbf86bececbe00a2793577961b5df59695d14451
Maurizio <maurizio.pietrantuono@gmail.com>
2015-05-06 09:30:51 +0200

    Working

--- app/build/generated/source/buildConfig/androidTest/debug/net/majorkernelpanic/spydroid/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/net/majorkernelpanic/spydroid/test/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.pietrantuono.test.test;
+package net.majorkernelpanic.spydroid.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.pietrantuono.test.test";
+  public static final String APPLICATION_ID = "net.majorkernelpanic.spydroid.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
